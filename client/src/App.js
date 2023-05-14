d0064306b39d3913c3c54ab2fc56caefb84b4ed3
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-05-14 16:32:18 +0600

    installed all required library in the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import { Button } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="bg-primary h-screen flex justify-center items-center">
-      <div className="bg-white p-5">
-        <h1>olduct</h1>
-        <Button type="primary">antD</Button>
-      </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
