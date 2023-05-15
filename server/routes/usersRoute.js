95fee2620188358c767bcaae9d00b235ff9aa842
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-05-15 14:54:12 +0600

    added userModel to specify the user data and create router to make the proper endpoint of the project

---
+++ server/routes/usersRoute.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// new user Register
+router.post("/register", async (req, res) => {
+  // always write a try..catch block for async/await or any endpoint
+  try {
+    // check that if the user already exists
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      throw new Error("User already exists");
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    // create new user
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.send({
+      success: true,
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// user login
+router.post("/login", async (req, res) => {
+  try {
+    // check if the user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    // check if the password is correct
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      throw new Error("Invalid password");
+    }
+
+    // create and assign a token
+    const token = jwt.sign({ useId: user._id }, process.env.jwt_secret);
+
+    // send response
+    res.send({
+      success: true,
+      message: "User logged in successfully",
+      data: token,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
