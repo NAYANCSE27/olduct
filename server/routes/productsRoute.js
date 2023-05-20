5be11e57ba7c530f5f167d59055c99cf084be661
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-05-20 22:27:29 +0600

    added CRUD part of the project and having some issues with database connection while rendering in the frontend

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -22,10 +22,42 @@ router.post("/add-product", authMiddleware, async (req, res) => {
 // Get all products
 router.get("/get-products", async (req, res) => {
   try {
-    const products = await Product.find();
+    const products = await Product.find().sort({ createdAt: -1 });
     res.send({
       success: true,
       products,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// Get a product by id
+router.put("/edit-product/:id", authMiddleware, async (req, res) => {
+  try {
+    await Product.findByIdAndUpdate(req.params.id, req.body);
+    res.send({
+      success: true,
+      message: "Product updated successfully",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// Delete a product by id
+router.delete("/delete-product/:id", authMiddleware, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.send({
+      success: true,
+      message: "Product deleted successfully",
     });
   } catch (error) {
     res.send({
