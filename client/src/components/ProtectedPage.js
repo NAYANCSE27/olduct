ab7a10ad9e33f94836d2c067245251674dbf2279
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-05-26 19:41:21 +0600

    added notification model but don't work properly

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
-import { message } from "antd";
+import { Avatar, Badge, message } from "antd";
 import { GetCurrentUser } from "../api/users";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { SetLoading } from "../redux/loadersSlice";
 import { SetUser } from "../redux/usersSlice";
+import Notifications from "./Notifications";
+import { GetAllNotifications } from "../api/notifications";
 
 function ProtectedPage({ children }) {
   // const [user, setUser] = React.useState({});
+  const [notifications = [], setNotifications] = React.useState([]);
+  const [showNotifications, setShowNotifications] = React.useState(false);
   const { user } = useSelector((state) => state.users);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -31,9 +35,26 @@ function ProtectedPage({ children }) {
     }
   };
 
+  const getNotifications = async () => {
+    try {
+      dispatch(SetLoading(true));
+      const response = await GetAllNotifications();
+      dispatch(SetLoading(false));
+      if (response.success) {
+        setNotifications(response.data);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      dispatch(SetLoading(false));
+      message.error(error.message);
+    }
+  };
+
   useEffect(() => {
     if (localStorage.getItem("token")) {
       validatedToken();
+      getNotifications();
     } else {
       navigate("/login");
     }
@@ -52,7 +73,7 @@ function ProtectedPage({ children }) {
           </h1>
 
           <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
-            <i className="ri-shield-user-line"></i>
+            {/* <i className="ri-shield-user-line"></i> */}
             <span
               className="underline cursor-pointer uppercase"
               onClick={() => {
@@ -65,6 +86,19 @@ function ProtectedPage({ children }) {
             >
               {user.name}
             </span>
+            <Badge
+              count={
+                notifications?.filter((notification) => !notification.read)
+                  .length
+              }
+              onClick={() => setShowNotifications(true)}
+              className="cursor-pointer"
+            >
+              <Avatar
+                size="medium"
+                icon={<i className="ri-notification-3-line"></i>}
+              />
+            </Badge>
             <i
               className="ri-logout-box-r-line ml-7"
               onClick={() => {
@@ -77,6 +111,15 @@ function ProtectedPage({ children }) {
 
         {/* body section */}
         <div className="p-5">{children}</div>
+
+        {
+          <Notifications
+            notifications={notifications}
+            reloadNotifications={setNotifications}
+            showNotifications={showNotifications}
+            setShowNotifications={setShowNotifications}
+          />
+        }
       </div>
     )
   );
