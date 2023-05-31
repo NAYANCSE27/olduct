3cea8be4905bdaf18a4f6bdd8f07109582dd2993
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-05-31 18:45:12 +0600

    added all the configuration file

--- server/server.js
+++ server/server.js
@@ -16,4 +16,15 @@ app.use("/api/products", productsRoute);
 app.use("/api/bids", bidsRoute);
 app.use("/api/notifications", notificationsRoute);
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+// render deployment page
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port node/express ${port}`));
